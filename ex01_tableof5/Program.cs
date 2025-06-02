42818d756e8e0f1c1d7bcfecb6fa7f4a4d368a6d
Hiago Franco <hiago.franco@students.assembly.pt>
2025-06-02 19:39:24 +0100

    primeiro

---
+++ ex01_tableof5/Program.cs
@@ -0,0 +1,20 @@
+﻿/*1. The application receives a number and returns the table of 5. User the for cycle;
+5 X 1 = 5
+5 X 2 = 10
+5 X 3 = 15
+5 X 4 = 20
+5 X 5 = 25
+5 X 6 = 30
+5 X 7 = 35
+5 X 8 = 40
+5 X 9 = 45
+5 X 10 = 50*/
+
+Console.WriteLine("Give me a number and i will return it multiplicated for 5:");
+string input1 = Console.ReadLine();
+int number = int.Parse(input1);
+
+Console.WriteLine($"You choice the number {number}.");
+int i = 1;
+for (i = ; i < 11; i++);
+
