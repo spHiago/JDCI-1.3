a288cc7f8ac030793e0aa6fa255935c64eac832f
Hiago <hiagofranco2604@gmail.com>
2025-06-05 22:29:33 +0100

    ex04 almost finished

---
+++ ex04_FullTree/Program.cs
@@ -0,0 +1,30 @@
+﻿/*4. The application draws a full tree with the character *. The number of lines is provided by 
+the user. User the for cycle; 
+Example for number 4: 
+      ** **
+    **** ****
+  ****** *****
+******** ******
+*/
+
+Console.WriteLine("Digite o numero de espaçamento Z e Y utilizados na arvore:");
+string input1 = Console.ReadLine();
+int number = int.Parse(input1);
+Console.WriteLine($"voce escolheu o numero {number}.");
+for (int linha = 1; linha < number; linha++)
+{
+    for (int space = 0; space < number - linha; space++)
+    {
+        Console.Write(" ");
+    }
+    for (int ast = 0; ast < linha; ast++)
+    {
+        Console.Write("*");
+    }
+    Console.Write(" ");
+    for (int astDir = 0; astDir < linha; astDir++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
--- ex04_FullTree/obj/Debug/net9.0/ex04_FullTree.AssemblyInfo.cs
+++ ex04_FullTree/obj/Debug/net9.0/ex04_FullTree.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ex03_HalfTree")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex04_FullTree")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b66a0059354f79f64f1f7e9b218b017a69ebf6c")]
-[assembly: System.Reflection.AssemblyProductAttribute("ex03_HalfTree")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ex03_HalfTree")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex04_FullTree")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex04_FullTree")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
