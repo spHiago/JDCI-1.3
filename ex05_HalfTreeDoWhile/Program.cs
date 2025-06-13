9df1254a69af44b9ab042d7e4412149910625259
Hiago <hiagofranco2604@gmail.com>
2025-06-13 10:32:42 +0100

    ex05 Finished

---
+++ ex05_HalfTreeDoWhile/Program.cs
@@ -0,0 +1,28 @@
+﻿/*3. The application draws half a tree with the character *. The number of lines is provided by 
+the user. Use the for cycle; 
+Example for number 4: 
+* 
+** 
+*** 
+****
+*/
+
+//5. The application is identical to application 3, using the Do/While cycle;
+
+Console.WriteLine("Por favor, digite um numero:");
+int linha = Convert.ToInt32(Console.ReadLine());
+
+int linhaAtual = 0;
+int i = 0;
+
+do
+{
+    i = 0;
+    do
+    {
+        Console.Write("*");
+        i++;
+    } while (i <= linhaAtual);
+    Console.WriteLine();
+    linhaAtual++;
+} while (linhaAtual < linha);
