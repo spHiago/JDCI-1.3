1c8d42b5d6a328f3d423264a47761bd0af97c9ed
Hiago <hiagofranco2604@gmail.com>
2025-06-13 10:44:08 +0100

    ex06Finished

---
+++ ex06_HalfTreeWhile/Program.cs
@@ -0,0 +1,27 @@
+﻿/*3. The application draws half a tree with the character *. The number of lines is provided by 
+the user. Use the for cycle; 
+Example for number 4: 
+* 
+** 
+*** 
+**** */
+
+//6. The application is identical to application 3, using the While cycle; 
+
+Console.WriteLine("Por favor, digite um numero:");
+int linhasMax = Convert.ToInt32(Console.ReadLine());
+
+int linhaAtual = 0;
+int i = 0;
+
+while(linhaAtual <= linhasMax)
+{
+    i = 0;
+    while (i <= linhaAtual)
+    {
+        Console.Write("*");
+        i++;
+    }
+    Console.WriteLine();
+    linhaAtual++;
+}
