c7e2c5cc09415aade872b92ab7ed5606b26d944a
Hiago Franco <hiagofranco2604@gmail.com>
2025-06-23 18:10:09 +0100

    ex08_Finished

---
+++ ex08_FullTreeOddNumbers/Program.cs
@@ -0,0 +1,43 @@
+﻿/*The application draws a full tree, with odd numbers per row, with the character *. The
+number of lines is provided by the user. User the for cycle;
+Example for number 4:
+ *
+ ***
+ *****
+*******
+*/
+
+using System;
+namespace FullTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of lines for the tree:");
+            int lines = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+
+            while (i <= lines)
+            {
+                int spaces = 0;
+                while (spaces < lines - i)
+                {
+                    Console.Write(" ");
+                    spaces++;
+                }
+                int astDir = 0;
+                while (astDir <= 2 * i)
+                {
+                    Console.Write("*");
+                    astDir++;
+                }
+                Console.WriteLine();
+                i++;
+
+            }
+
+        }
+    }
+}
+
