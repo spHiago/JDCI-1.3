1002f7202b65c75153ef8a69545d04b619499b0e
Hiago Franco <hiagofranco2604@gmail.com>
2025-06-23 18:22:55 +0100

    ex09_Finished

---
+++ ex09_FullTreeStarsEqualLines/Program.cs
@@ -0,0 +1,41 @@
+﻿
+/*The application draws a full tree, with equal characters to the row, with the character *.
+The number of lines is provided by the user. User the for cycle;
+Example for number 4:
+ *
+ * *
+* * *
+* * * *
+*/
+
+
+using System;
+
+namespace FullTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of lines for the tree:");
+            int lines = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= lines; i++)
+            {
+               for (int space = 0;  space < lines; space++)
+                {
+                    Console.Write(" ");
+                }
+               for (int star = 0; star < i; star++)
+                {
+                    Console.Write("*");
+                    if (star < i - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+               Console.WriteLine();
+            }
+        }
+    }
+}
